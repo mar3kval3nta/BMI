f9817001a4ae28d1662f893c0e53f2e14aad54f9
Marek Valenta <Valenta.Marek@stud.vassboskovice.cz>
2021-10-19 11:33:00 +0200

    BMI

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -12,22 +12,20 @@ namespace BMI
 
             double hmotnost;
             double výška;
-            
             double BMI;
 
             Console.Write("Zadej hmotnost : ");
             double.TryParse(Console.ReadLine(), out hmotnost);
-            Console.ReadLine();
 
             Console.Write("Zadej výšku : ");
             double.TryParse(Console.ReadLine(), out výška);
-            Console.ReadLine();
 
             BMI = (hmotnost) / (výška/100 * výška/100);
             Console.WriteLine();
             BMI = Math.Round(BMI, 2);
             Console.WriteLine($"Vaše BMI : {BMI}");
             Console.WriteLine();
+            Console.ReadKey();
             
 
 
